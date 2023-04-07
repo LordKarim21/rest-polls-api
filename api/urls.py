c0ed70e630bac9a7a226bcbde33df6bbb1ea5597
User <ismail.karim@inbox.ru>
2023-04-08 01:39:19 +0600

    added polls api

--- api/urls.py
+++ api/urls.py
@@ -5,17 +5,19 @@ from rest_framework.routers import DefaultRouter
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-
 from polls.views import PollViewSet, QuestionViewSet, VoteViewSet
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Poll API",
+      title="Snippets API",
       default_version='v1',
-      description="Fabrique test task"
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
    ),
    public=True,
-   permission_classes=(permissions.AllowAny,),
+   permission_classes=[permissions.AllowAny],
 )
 
 router = DefaultRouter()
@@ -24,10 +26,7 @@ router.register(r'questions', QuestionViewSet)
 router.register(r'votes', VoteViewSet)
 
 urlpatterns = [
-    url(r'^v1/', include((router.urls, 'v1'), namespace='v1')),
-    url(
-        r'^swagger(?P<format>\.json|\.yaml)$',
-        schema_view.without_ui(cache_timeout=0),
-        name='schema-json'
-    ),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
 ]
